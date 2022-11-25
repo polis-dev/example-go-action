4260a1970b3da337a0b14cb3f084b01aae4aaeb0
Jake <820715+jakelogemann@users.noreply.github.com>
2022-11-25 13:56:16 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module gogha
+
+go 1.19
+
+require (
+	github.com/sethvargo/go-envconfig v0.8.2 // indirect
+	github.com/sethvargo/go-githubactions v1.1.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/sethvargo/go-githubactions"
+)
+
+func main() {
+  actions := githubactions.WithFieldsMap(map[string]string{
+    "file": "myfile.js",
+    "line": "100",
+  })
+
+  val := actions.GetInput("val")
+  if val == "" {
+    actions.Fatalf("missing 'val'")
+  }
+}
